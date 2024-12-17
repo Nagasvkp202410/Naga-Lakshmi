39267c57b075ec8156363f936c17f3d0cfa0400d
LPT-14 <LPT-14@10.0.0.109>
2024-12-17 15:20:06 -0800

    first

---
+++ SVKP_college_next_finish/src/org/tnsif/capgemini/c2tc/finaldemo/Finalclass.java
@@ -0,0 +1,17 @@
+package org.tnsif.capgemini.c2tc.finaldemo;
+
+ final class calc //hema
+{
+	public void show()
+	{
+		System.out.println("AI integration done by hema");
+	}
+}
+
+ class adav extends  calc //yasar
+{
+	public void show()
+	{
+		System.out.println("AI integration done by yasar");
+	}
+}
